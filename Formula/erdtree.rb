bc783e04d032ddc0a5ea95d36737b0eff58994c9
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-05-08 18:55:27 -0700

    fix sha256

--- Formula/erdtree.rb
+++ Formula/erdtree.rb
@@ -2,7 +2,7 @@ class Erdtree < Formula
   desc "Colorful file tree visualizer for the terminal with memory sizes adjacent."
   homepage "https://github.com/solidiquis/erdtree"
   url "https://github.com/solidiquis/erdtree/releases/download/v0.1.0/erdtree-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "907a06d8591eaf8204299b88b8533d491e7bf4fc"
+  sha256 "2b10822721164fd467a123565841a3388b87553d9791af34ebacce8f7fec8665"
   version "0.1.0"
 
   def install
