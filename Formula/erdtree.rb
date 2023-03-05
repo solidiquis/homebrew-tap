189d81815bbd07eed6d4c9dce54dc8565fd31951
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-03-04 21:27:11 -0800

    et version bump

--- Formula/erdtree.rb
+++ Formula/erdtree.rb
@@ -1,15 +1,15 @@
 class Erdtree < Formula
-  version "1.1.0"
+  version "1.2.0"
   desc "erdtree (et) is a multi-threaded filetree visualizer and disk usage analyzer."
   homepage "https://github.com/solidiquis/erdtree"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/solidiquis/erdtree/releases/download/v#{version}/et-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "14382652e8474c19aaec1ca1874da9a9a6c28f18e82a777c901d492e536bf909"
+    sha256 "9011e116e041dad7137a45f64042532e2ca79bc6c610237e8e0b5088fc35c1cb"
   elsif OS.linux?
     url "https://github.com/solidiquis/erdtree/releases/download/v#{version}/et-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "db30d05c073bed3dde34f6273dbec5986fb52fa564f111ef467a65ee44fb5653"
+    sha256 "59865541ecae8eb165ecf97496d0bebb4f4edccb45822b2f7b4fd244968a6346"
   end
 
   conflicts_with "erdtree"
